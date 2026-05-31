6d33061423fe4ba5049f8e86a24ae679ff4bac31
GuillaumeJmt <guillaume.jumet@gmail.com>
2026-05-31 11:40:15 +0200

    add: Lmod modulefile for NWChem 7.3.0 with validated water HF/STO-3G calculation

---
+++ modulefiles/NWChem/7.3.0.lua
@@ -0,0 +1,23 @@
+help([[
+NWChem 7.3.0 - Open Source High-Performance Computational Chemistry
+
+Usage:
+  nwchem input.nw > output.log
+
+Documentation: https://nwchemgit.github.io
+]])
+
+whatis("Name:        NWChem")
+whatis("Version:     7.3.0")
+whatis("Category:    Computational Chemistry")
+whatis("Description: Open-source high-performance computational chemistry")
+whatis("URL:         https://nwchemgit.github.io")
+
+local home = "/home/guillaumelumin.guest/miniconda3"
+local version = "7.3.0"
+
+prepend_path("PATH",            home .. "/bin")
+prepend_path("LD_LIBRARY_PATH", home .. "/lib")
+
+setenv("NWCHEM_BASIS_LIBRARY", home .. "/share/nwchem/libraries.bse/")
+setenv("NWCHEM_VERSION",       version)
